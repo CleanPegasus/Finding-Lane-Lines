c16cce0b665057d85efaa5863600338bd78a61d3
Arunkumar L <arunk609@gmail.com>
2018-11-22 10:05:58 +0530

    Completed Lane Finding in self driving cars.

--- lane_lines.py
+++ lane_lines.py
@@ -1,65 +1,99 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 
+def make_coordinates(image, line):
+    slope, intercept = line
+    y1 = int(image.shape[0])
+    y2 = int(y1 * 3 / 5)
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return [[x1, y1, x2, y2]]
 
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    canny = cv2.Canny(blur, 50, 150)
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    if lines is None:
+        return None
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+    # add more weight to longer lines
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    averaged_lines = [left_line, right_line]
+    return averaged_lines
+
+
+def canny(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    kernel = 5
+    blur = cv2.GaussianBlur(gray, (kernel, kernel), 0)
+    canny = cv2.Canny(gray, 50, 150)
     return canny
 
 
-def display_lines(image, lines):
-    line_image = np.zeros_like(image)
+def display_lines(img, lines):
+    line_image = np.zeros_like(img)
     if lines is not None:
         for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
 
-def reg_of_interest(image):
-    #    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    #    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    #    canny = cv2.Canny(blur, 50, 150)
+def reg_of_interest(canny):
+    height = canny.shape[0]
+    width = canny.shape[1]
+    mask = np.zeros_like(canny)
 
-    height = image.shape[0]
-    polygons = np.array([
-        [(200, height), (1000, height), (500, 250)]
-    ])
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
+    triangle = np.array([[
+        (200, height),
+        (550, 250),
+        (1100, height), ]], np.int32)
 
-    masked_img = cv2.bitwise_and(image, mask)
-
-    return masked_img
-
-image = cv2.imread('test_image.jpg')
-lane_img = np.copy(image)
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(canny, mask)
+    return masked_image
 
 
-canny = canny(lane_img)
-cropped_image = reg_of_interest(canny)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
-line_image = display_lines(lane_img, lines)
-combo_image = cv2.addWeighted(lane_img, 0.8, line_image, 1, 1)
-#cv2.imshow("result", masked_img)
-plt.imshow(cropped_image)
+# image = cv2.imread('test_image.jpg')
+# lane_image = np.copy(image)
+# lane_canny = canny(lane_image)
+# cropped_canny = region_of_interest(lane_canny)
+# lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+# averaged_lines = average_slope_intercept(image, lines)
+# line_image = display_lines(lane_image, averaged_lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('masked_img', 600, 600)
-cv2.namedWindow('masked_img', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 600, 600)
-cv2.namedWindow('canny', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('canny', 600, 600)
+#
+cap = cv2.VideoCapture("test2.mp4")
 
-cv2.imshow("result", combo_image)
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (600,480))
 
-cv2.imshow("image", lane_img)
-cv2.imshow("canny", canny)
-cv2.imshow("line_image", line_image)
 
-cv2.waitKey(0)
+while (cap.isOpened()):
+    _, frame = cap.read()
+    canny_image = canny(frame)
+    cropped_canny = reg_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_canny, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow("result", combo_image)
+    output.write(combo_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+output.release()
+cv2.destroyAllWindows()
